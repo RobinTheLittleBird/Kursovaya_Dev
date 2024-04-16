1fa8e1d1d5aa61b686d9e34c8358e96edda5d514
Ekaterina <95190787+RobinTheLittleBird@users.noreply.github.com>
2024-04-16 23:21:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World! Work pls pls'));
+app.get('/', (req, res) => res.send('Hello World! Work pls pls pls'));
 
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
