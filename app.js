a689101803f08e9e36b573ee17fbd322d23c14a7
Ekaterina <95190787+RobinTheLittleBird@users.noreply.github.com>
2024-04-17 23:31:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const port = 80;
 app.use(express.json());
 
 
-app.get('/', (req, res) => res.send('Hello World! work? pls...'));
-
-
-// Обработчик маршрута "/days-to-new-year" для вывода количества дней до Нового года
-app.get('/days-to-new-year', (req, res) => {
+app.get('/', (req, res) => {
   const today = new Date();
   const newYear = new Date(today.getFullYear() + 1, 0, 1); // Дата Нового года следующего года
   const daysLeft = Math.floor((newYear - today) / (1000 * 60 * 60 * 24)); // Вычисляем количество дней до Нового года
-  res.send(`Days left until New Year: ${daysLeft}`);
+
+  // Отправляем ответ с объединенными данными
+  res.send(`Hello World! Days left until New Year: ${daysLeft}`);
 });
 
 
