43aa34fd11b126b8ae882f0b9064c04a29871bc5
Ekaterina <95190787+RobinTheLittleBird@users.noreply.github.com>
2024-04-17 23:08:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World! Work pls pls pls'));
+// Middleware для обработки JSON данных в теле запроса
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+// Обработчик маршрута "/days-to-new-year" для вывода количества дней до Нового года
+app.get('/days-to-new-year', (req, res) => {
+  const today = new Date();
+  const newYear = new Date(today.getFullYear() + 1, 0, 1); // Дата Нового года следующего года
+  const daysLeft = Math.floor((newYear - today) / (1000 * 60 * 60 * 24)); // Вычисляем количество дней до Нового года
+  res.send(`Days left until New Year: ${daysLeft}`);
+});
 
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
